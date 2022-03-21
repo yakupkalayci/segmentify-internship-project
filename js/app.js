ad48078743a9e8b581a72d8a710b51101f67912f
yakupkalayci <ykpklyc18@gmail.com>
2022-03-21 19:52:14 +0300

    add swiperjs library

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    autoHeight: true,
+    slidesPerView: 4,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
