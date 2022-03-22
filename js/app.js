e7943ec6733061f53cb489f5fbd3d29bf7fee6db
yakupkalayci <ykpklyc18@gmail.com>
2022-03-22 20:24:38 +0300

    fetch products

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
 const swiper = new Swiper(".swiper", {
-    // Optional parameters
-    direction: "horizontal",
-    autoHeight: true,
-    slidesPerView: 4,
+  // Optional parameters
+  direction: "horizontal",
+  autoHeight: true,
+  slidesPerView: 4,
 
-    // Navigation arrows
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Responsive Breakpoints
+  breakpoints: {
+    250: {
+       slidesPerView: 1,
     },
-    // Responsive Breakpoints
-    breakpoints: {
-      250: {
-         slidesPerView: 1,
-      },
-      480: {
-        slidesPerView: 2,
-      },
-      710: {
-        slidesPerView: 3,
-      },
-      768: {
-        slidesPerView: 4,
-      },
+    480: {
+      slidesPerView: 2,
     },
+    710: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+  },
 });
 
 const categories = document.querySelector("#categories");
@@ -75,3 +75,85 @@ getData()
 </div>`
   });
 })
+.catch((err) => console.error(err));
+
+categories.addEventListener("click", e => {
+
+  if(e.target.id !== "categories") {
+    let current = document.querySelector(".active");
+    current && current.classList.remove("active");
+    e.target.classList.add("active");
+  }
+
+  getData()
+  .then(response => {
+
+    let { products, userCategories } = response;
+    productsContainer.innerHTML = "";
+    changeTabTitle(e.target.textContent, userCategories);
+
+    function getKey(categoryName) {
+      let value = categoryName;
+      Object.keys(products).forEach((key) => {
+        if (key.includes(">")) {
+          let index = key.indexOf(">");
+          if (key.slice(index + 2) == categoryName) {
+            value = key;
+          }
+        }
+      });
+      return value;
+    }
+
+    products[`${getKey(e.target.textContent)}`].forEach(product => {
+      productsContainer.innerHTML += `
+          <div class="swiper-slide">
+          <div class="product">
+              <div class="productImg">
+                  <img src=${
+                    product.image
+                  } width="200px" height="200px" loading="lazy">
+              </div>
+              <h3 class="productTitle">${
+                product.name.length > 51
+                  ? product.name.slice(
+                      0,
+                      product.name.length - (product.name.length - 51)
+                    ) + "..."
+                  : product.name
+              }</h3>
+              <p class="price">${product.priceText}</p>
+              ${
+                product.params.shippingFee === "FREE"
+                  ? `<div class="shipping">
+                  <div id="icon">
+                    <i class="fa-solid fa-truck"></i>
+                  </div>
+                  <div>
+                    <p>Ücretsiz Kargo</p>
+                  </div>
+                </div>`
+                  : ""
+              }
+          </div>
+    </div>
+          `;
+    });
+  })
+  .catch(err => console.error(err));
+})
+
+function changeTabTitle(selected, categories) {
+  const tabDOM = document.querySelector("#header>h1");
+
+  categories.forEach((category) => {
+    if (category.includes(">")) {
+      let index = category.indexOf(">");
+      if (category.slice(index + 2) == selected) {
+        tabDOM.innerHTML = category;
+      }
+    } else {
+      tabDOM.innerHTML = "Sizin için Seçtiklerimiz";
+    }
+  });
+}
